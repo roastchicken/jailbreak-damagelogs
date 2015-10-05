6e201136c3513eafcbc7bc22227fca0b44ab8870
thatoneprogrammerkid <thatoneprogrammerkid@gmail.com>
2015-10-05 12:00:08 -0500

    moved coloredbox.lua

--- lua/damagelogs/sv_damagelog.lua
+++ lua/damagelogs/sv_damagelog.lua
@@ -1,5 +1,5 @@
 AddCSLuaFile("damagelogs/cl_damagelog.lua")
-AddCSLuaFile("vgui/coloredbox.lua")
+AddCSLuaFile("damagelogs/not_my_code/coloredbox.lua")
 AddCSLuaFile("damagelogs/cl_tabs/damagetab.lua")
 AddCSLuaFile("damagelogs/cl_tabs/settings.lua")
 AddCSLuaFile("damagelogs/cl_tabs/shoots.lua")
